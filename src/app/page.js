c5935a6c5915be9d5d0352f1189d287397ab1578
mranv <iamanubhavgain@gmail.com>
2024-04-24 03:47:35 +0530

    Added: initial commit & team members details.

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+
+
+import LampDemo from "@/components/LampDemo";
+import TeamCard from "@/components/TeamCard";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <LampDemo />
+      <TeamCard />
+    </>
+  );
+}
